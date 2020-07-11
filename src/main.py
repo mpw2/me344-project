54bbbb9c724e1d29452104e3af2f2baa82dc19cd
Michael Whitmore <mpw2@stanford.edu>
2020-07-11 15:09:15 -0700

    create some initial file structure

---
+++ src/main.py
@@ -0,0 +1,27 @@
+#------------------------------------------------
+#
+# Finite Volume Jet Code
+#
+# Authors: Carlos Gonzalez, Michael Whitmore
+#
+# ... Solver details
+#
+#------------------------------------------------
+
+
+def main():
+   # Read user input file
+   read_input()
+   
+   # Initialize data structures
+   initialize()
+   
+   # Start time integration loop
+   for n in range(Nt):
+        
+    
+    
+
+if __name__ == "__main__":
+    main()
+
