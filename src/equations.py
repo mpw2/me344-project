77c94bb32b9e83ceddb15f9bb72aa9917a70b2a7
Carlos Gonzalez <gonzalez.a.carlos09@gmail.com>
2020-07-14 16:45:45 -0700

    Added heat flux term, changed indexing in compute_deriv functions

--- src/equations.py
+++ src/equations.py
@@ -12,15 +12,15 @@ def compute_x_deriv(phi, x, y, dir_id):
 	hi_y = ny-1
 	dphi = np.zeros((nx,ny))
 	if dir_id == 0:
-		dphi[nx,:] = (phi[nx,:] - phi[nx-1,:]) / (x[nx,:] - x[nx-1,:])
-		dphi[0:hi_x,:] = (phi[0+1:hi_x+1,:] - phi[0:hi_x, :]) / (x[0+1:hi_x+1,:] - x[0:hi_x,:])
+		dphi[nx,:] = (phi[nx,:] - phi[nx-1,:]) / (x[nx] - x[nx-1])
+		dphi[0:hi_x,:] = (phi[0+1:hi_x+1,:] - phi[0:hi_x]) / (x[1:hi_x+1,:] - x[0:hi_x])
 	if dir_id = 1:
-		dphi[0,:] = (phi[1,:] - phi[0,:]) / (x[1,:] - x[0,:])
-		dphi[lo_x:nx,:] = (phi[lo_x:hi_x,:] - phi[lo_x-1:hi_x-1, :]) / (x[lo_x:hi_x,:] - x[lo_x-1:hi_x-1,:]) 
+		dphi[0,:] = (phi[1,:] - phi[0,:]) / (x[1] - x[0])
+		dphi[lo_x:nx,:] = (phi[lo_x:hi_x,:] - phi[lo_x-1:hi_x-1, :]) / (x[lo_x:hi_x] - x[lo_x-1:hi_x-1]) 
 	if dir_id = 2:
 		dphi[0,:] = (phi[1,:] - phi[0,:]) / (x[1,:] - x[0,:])
 		dphi[nx,:] = (phi[nx,:] - phi[nx-1,:]) / (x[nx,:] - x[nx-1,:])
-		dphi[lo_x:hi_x,:] = (phi[lo_x+1:hi_x+1, :] - phi[lo_x-1:hi_x-1, :]) / (x[lo_x+1:hi_x+1,:] - x[lo_x-1:hi_x-1,:]) 
+		dphi[lo_x:hi_x,:] = (phi[lo_x+1:hi_x+1, :] - phi[lo_x-1:hi_x-1, :]) / (x[lo_x+1:hi_x+1] - x[lo_x-1:hi_x-1]) 
 	return dphi	
 
 def compute_y_deriv(phi, x, y, dir_id):
@@ -32,19 +32,19 @@ def compute_y_deriv(phi, x, y, dir_id):
 	hi_y = ny-1
 	dphi = np.zeros((nx,ny))
 	if dir_id == 0:
-		dphi[:,ny] = (phi[:,ny] - phi[:,ny-1]) / (y[:,ny] - y[:,ny-1])
-		dphi[:,0:hi_y] = (phi[:,0+1:hi_y+1] - phi[:, 0:hi_y]) / (y[:, 0:hi_y+1] - y[:, 0:hi_y]) 
+		dphi[:,ny] = (phi[:,ny] - phi[ny-1]) / (y[ny] - y[ny-1])
+		dphi[:,0:hi_y] = (phi[:,0+1:hi_y+1] - phi[:, 0:hi_y]) / (y[0:hi_y+1] - y[0:hi_y]) 
 	if dir_id = 1:
-		dphi[:,0] = (phi[:,1] - phi[:,0]) / (y[:,1] - y[:,0])
-		dphi[:,lo_y:ny] = (phi[:,lo_y:ny] - phi[:, lo_y-1:ny-1]) / (y[:, lo_y:ny] - y[:, lo_y-1:ny-1]) 
+		dphi[:,0] = (phi[:,1] - phi[:,0]) / (y[1] - y[0])
+		dphi[:,lo_y:ny] = (phi[:,lo_y:ny] - phi[:, lo_y-1:ny-1]) / (y[lo_y:ny] - y[lo_y-1:ny-1]) 
 	if dir_id = 2:
-		dphi[:,0] = (phi[:,1] - phi[:,0]) / (y[:,1] - y[:,0])
-		dphi[:,ny] = (phi[:,ny] - phi[:,ny-1]) / (y[:,ny] - y[:,ny-1])
-		dphi[:,lo_y:hi_y] = (phi[:, lo_y+1:hi_y+1] - phi[: lo_y+1:hi_y+1]) / (y[:, lo_y+1:hi_y+1] - y[:, lo_y-1:hi_y-1]) 
+		dphi[:,0] = (phi[:,1] - phi[:,0]) / (y[1] - y[0])
+		dphi[:,ny] = (phi[:,ny] - phi[:,ny-1]) / (y[ny] - y[ny-1])
+		dphi[:,lo_y:hi_y] = (phi[:, lo_y+1:hi_y+1] - phi[: lo_y+1:hi_y+1]) / (y[lo_y+1:hi_y+1] - y[lo_y-1:hi_y-1]) 
 	return dphi	
 
 def ConsToPrim(Q,gamma):
-        Rho_ = Q[:,:,0]
+    Rho_ = Q[:,:,0]
 	U_ = Q[:,:,1] / Q[:,:,0]
 	V_ = Q[:,:,2] / Q[:,:,0]
 	P_ = (gamma - 1) * (Q[:,:,3] - 1 / 2 / Q[:,:,0] * (Q[:,:,1] + Q[:,:,2])**2)
@@ -52,11 +52,11 @@ def ConsToPrim(Q,gamma):
 	return Rho_, U_, V_, P_
 
 def PrimToCons(Rho,U,V,P):
-	rhoU = Rho * U
-	rhoV = Rho * V
-	Et = P / (gamma - 1) + Rho / 2 * (U**2 + V**2)
+	rhoU_ = Rho * U
+	rhoV_ = Rho * V
+	Et_ = P / (gamma - 1) + Rho / 2 * (U**2 + V**2)
 
-	return Rho, rhoU, rhoV, Et
+	return Rho, rhoU_, rhoV_, Et_
 
 def Tauxx(U,x,y,mu,step):
 
@@ -82,10 +82,29 @@ def Tauyy(V,x,y,mu,step):
 
 	return tau_yy
 
+def Qx(T,x,y,k,step):
+
+	if step == 'predictor':
+		qx = -1 * k * compute_x_deriv(T,x,y,1)
+	elif step == 'corrector':
+		qx = -1 * k * compute_x_deriv(T,x,y,0)
+	else:
+		raise Exception('Invalid Step')
+
+	return qx
+
+def Qy(T,x,y,k,step):
+
+	if step == 'predictor':
+		qy = -1 * k * compute_y_deriv(T,x,y,1)
+	elif step == 'corrector':
+		qy = -1 * k * compute_y_deriv(T,x,y,0)
+	else:
+		raise Exception('Invalid Step')
+
+	return qy
+
 def Tauxy(U,V,x,y,mu,flux_dir,step):
-	nx = np.shape(x,0)
-	ny = np.shape(y,1)
-	tau_xy = np.zeros((nx,ny))
 
 	if step == 'predictor':
 		if flux_dir == 0:
@@ -109,37 +128,43 @@ def Tauxy(U,V,x,y,mu,flux_dir,step):
 
 	return tau_xy
 
-def compE(Q,x,y,mu,gamma,step):
+def compE(Q,x,y,Rgas,mu,kappa,gamma,step):
 	nx = np.shape(x,0)
 	ny = np.shape(y,1)
 	E = np.zeros([nx,ny,nvars])
 
 	Rho, U, V, P = ConsToPrim(Q,gamma)
 
+	T = P / (Rho * Rgas)
+
 	tau_xx = Tauxx(U,x,y,mu,step)
 	tau_xy = Tauxy(U,V,x,y,mu,0,step)
+	qx = Qx(T,x,y,kappa,step)
 
 	E[:,:,0] = Rho * U
 	E[:,:,1] = Rho * U**2 + P - tau_xx
 	E[:,:,2] = Rho * U * V - tau_xy
-	E[:,:,3] = (Q[:,:,-1] + P) * U - U * tau_xx - V * tau_xy
+	E[:,:,3] = (Q[:,:,-1] + P) * U - U * tau_xx - V * tau_xy + qx
 
 	return E
 
-def compF(Q,x,y,mu,gamma,step):
+def compF(Q,x,y,Rgas,mu,kappa,gamma,step):
 	nx = np.shape(x,0)
 	ny = np.shape(y,0)
 	F = np.zeros([nx,ny,nvars])
 
 	Rho, U, V, P = ConsToPrim(Q,gamma)
 
+	T = P / (Rho * Rgas)
+
 	tau_yy = Tauyy(V,x,y,mu,step)
 	tau_xy = Tauxy(U,V,x,y,mu,1,step)
+	qy = Qy(T,x,y,kappa,step)
 
 	F[:,:,0] = Rho * V
 	F[:,:,1] = Rho * U * V - tau_xy
 	F[:,:,2] = Rho * V**2 + P - tau_yy
-	F[:,:,3] = (Q[:,:,-1] + P) * V - U * tau_xy - V * tau_yy
+	F[:,:,3] = (Q[:,:,-1] + P) * V - U * tau_xy - V * tau_yy + qy
 
 	return F
 
