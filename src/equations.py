7afb3d01f8c8c30228c991a92d0927e4c95d53ab
Carlos Gonzalez <gonzalez.a.carlos09@gmail.com>
2020-07-13 18:58:21 -0700

    Added many utility functions, computation of rhs
    Currently, size of tau doesn't match size of E and F so the addition won't work.

--- src/equations.py
+++ src/equations.py
@@ -3,38 +3,20 @@ from global import xg
 from global import nx 
 from global import mu
 from global import gamma
+from global import nvars
 
-# Compute RHS for drho/dt
-
-def compute_rhs_rho(Rho,U,rhs_rho,dir):
-
-	# only apply on interior of domain, BCs handled elsewhere
-
-	if dir == 'forward':
-		for ii in range(1,nx-1):
-			term[ii] = -1 * (Rho[ii+1] * U[ii+1] - Rho[ii] * U[ii]) / (xg[ii+1] - xg[ii])
-	elif dir == 'backward':
-		for ii in range(1,nx-1):
-			term[ii] = -1 * (Rho[ii] * U[ii] - Rho[ii-1] * U[ii-1]) / (xg[ii] - xg[ii-1])
-	else:
-		for ii in range(1,nx-1):
-			term[ii] = -1 * (Rho[ii+1] * U[ii+1] - Rho[ii-1] * U[ii-1]) / (xg[ii+1] - xg[ii-1])
-
-	rhs_rho[1:nx-1] += term
-
-	return rhs_rho
 
 # dir_id: ( 0 - Forward, 1 - Backward, 2 - Central )
 def compute_x_deriv(phi, x, y, dir_id):
-	nx = np.length(x)
-	ny = np.length(y)
+	nx = np.shape(x,0)
+	ny = np.shape(y,1)
 	lo_x = 1
 	hi_x = nx-1
 	lo_y = 1
 	hi_y = ny-1
 	dphi = np.zeros((nx-2,ny-2))
 	if dir_id == 0:
-		dphi = (phi[lo_x+1:hi_x+1,lo_y:hi_y] - phi[lo_x:hi_x, lo_y:hi_y]) / (x[lo_x+1:hi_x+1,lo_y:hi_y] - x[lo_x:hi_x,lo_y:hi_y)] 
+		dphi = (phi[lo_x+1:hi_x+1,lo_y:hi_y] - phi[lo_x:hi_x, lo_y:hi_y]) / (x[lo_x+1:hi_x+1,lo_y:hi_y] - x[lo_x:hi_x,lo_y:hi_y])
 	if dir_id = 1:
 		dphi = (phi[lo_x:hi_x,lo_y:hi_y] - phi[lo_x-1:hi_x-1, lo_y:hi_y]) / (x[lo_x:hi_x,lo_y:hi_y] - x[lo_x-1:hi_x-,lo_y:hi_y1]) 
 	if dir_id = 2:
@@ -42,8 +24,8 @@ def compute_x_deriv(phi, x, y, dir_id):
 	return dphi	
 
 def compute_y_deriv(phi, x, y, dir_id):
-	nx = np.length(x)
-	ny = np.length(y)
+	nx = np.shape(x,0)
+	ny = np.shape(y,0)
 	lo_x = 1
 	hi_x = nx-1
 	lo_y = 1
@@ -57,82 +39,193 @@ def compute_y_deriv(phi, x, y, dir_id):
 		dphi = (phi[lo_x:hi_x, lo_y+1:hi_y+1] - phi[lo_x:hi_x, lo_y+1:hi_y+1]) / (y[lo_x:hi_x, lo_y+1:hi_y+1] - y[lo_x:hi_x, lo_y-1:hi_y-1]) 
 	return dphi	
 
-def compute_rhs_rhoU(Rho,U,P,rhs_rhoU,dir):
+def ConsToPrim(Q,gamma):
+	Rho = Q[:,:,0]
+	U = Q[:,:,1] / Q[:,:,0]
+	V = Q[:,:,2] / Q[:,:,0]
+	P = (gamma - 1) * (Q[:,:,3] - 1 / 2 / Q[:,:,0] * (Q[:,:,1] + Q[:,:,2])**2)
 
-	term1 = np.array([nx-2,1])
-	term2 = np.array([nx-2,1])
-	term3 = np.array([nx-2,1])
+	return Rho, U, V, P
 
-	# only apply on interior of domain, BCs handled elsewhere
-
-	if dir == 'forward':
-		for ii in range(1,nx-1):
-			term1[ii] = -1 * (Rho[ii+1] * U[ii+1]**2 - Rho[ii] * U[ii]**2) / (xg[ii+1] - xg[ii]) # rho u^2
-			term2[ii] = -1 * (P[ii+1] - P[ii]) / (xg[ii+1] - xg[ii]) # pressure
-			term3[ii] = 2 * mu * (U[ii+1] - 2 * U[ii] + U[ii-1]) / (xg[ii+1] - xg[ii-1])**2 # d/dx(tau_xx) 
-	elif dir == 'backward':
-		for ii in range(1,nx-1):
-			term1[ii] = -1 * (Rho[ii+1] * U[ii+1]**2 - Rho[ii-1] * U[ii-1]**2) / (xg[ii+1] - xg[ii-1]) # rho u^2
-			term2[ii] = -1 * (P[ii+1] - P[ii-1]) / (xg[ii+1] - xg[ii-1]) # pressure
-			term3[ii] = 2 * mu * (U[ii+1] - 2 * U[ii] + U[ii-1]) / (xg[ii+1] - xg[ii-1])**2 # d/dx(tau_xx) 
-	else:
-		for ii in range(1,nx-1):
-			term1[ii] = -1 * (Rho[ii+1] * U[ii+1]**2 - Rho[ii-1] * U[ii-1]**2) / (xg[ii+1] - xg[ii-1]) # rho u^2
-			term2[ii] = -1 * (P[ii+1] - P[ii-1]) / (xg[ii+1] - xg[ii-1]) # pressure
-			term3[ii] = 2 * mu * (U[ii+1] - 2 * U[ii] + U[ii-1]) / (xg[ii+1] - xg[ii-1])**2 # d/dx(tau_xx) 
-
-	rhs_rhoU[1:nx-1] += term1 + term2 + term3
-
-	return rhs_rhoU
-
-def compute_rhs_Et(Et,U,P,rhs_Et):
-
-	term1 = np.array([nx-2,1])
-	term2 = np.array([nx-2,1])
-
-	for ii in range(1,nx-1):
-		term1[ii] = U[ii] * ( (Et[ii+1] - Et[ii-1]) / (xg[ii+1] - xg[ii-1]) + (P[ii+1] - P[ii-1]) / (xg[ii+1] - xg[ii-1]) ) +\
-		(Et[ii] + P[ii]) * (U[ii+1] - U[ii-1]) / (xg[ii+1] - xg[ii-1]) # d/dx ((Et + p) * u)
-		term2[ii] = 2 * mu * ( ((U[ii+1] - U[ii-1]) / (xg[ii+1] - xg[ii-1]))**2 +  U[ii] * \
-		(U[ii+1] - 2 * U[ii] + U[ii-1]) / (xg[ii+1] - xg[ii-1])**2 ) # d/dx (u * tau_xx)
-
-	rhos_E[1:nx-1] += term1 + term2
-
-	return rhos_Et
-
-def Prim2Con(Rho,U,P):
+def PrimToCons(Rho,U,V,P):
 	rhoU = Rho * U
-	Et = P / (gamma - 1) + Rho / 2 * (U**2)
+	rhoV = Rho * V
+	Et = P / (gamma - 1) + Rho / 2 * (U**2 + V**2)
 
-	return Rho, rhoU, Et
+	return Rho, rhoU, rhoV, Et
 
-def Tauxx(U,step):
+def Tauxx(U,x,y,mu,step):
 
-	lo = 1
-	hi = nx-1
+	if step == 'predictor':
+		tau_xx = 2 * mu * compute_x_deriv(U,x,y,1)
 
-	(U[lo+1:hi+1, ;, 1] - U[lo:hi,:,1]) / (xg[] - xg[])
+	elif step == 'corrector':
+		tau_xx = 2 * mu * compute_x_deriv(U,x,y,0)
 
-	tau_xx = np.array([nx,1])
-	for ii in range(1,nx-1):
-		if ii == 0:
-			tau_xx[ii] = (U[ii+1] - U[ii]) / (xg[ii+1] - xg[ii])
-		elif ii == nx:
-			tau_xx[ii] = (U[ii] - U[ii-1]) / (xg[ii] - xg[ii-1])
-		else:
-			tau_[xx] = (U[ii + 1] - U[ii - 1]) / (xg[ii + 1] - xg[ii - 1])
+	else:
+		raise Exception('Invalid Step')
+
 	return tau_xx
 
-def compE(U):
+def Tauyy(V,x,y,mu,step):
 
-def compRHS(U,step):
-
-	# format of U[x,y,1-4] 
 	if step == 'predictor':
+		tau_yy = 2 * mu * compute_y_deriv(V,x,y,1)
+	elif step == 'corrector':
+		tau_yy = 2 * mu * compute_y_deriv(V,x,y,0)
+	else:
+		raise Exception('Invalid Step')
+
+	return tau_yy
+
+def Tauxy(U,V,x,y,mu,flux,step):
+
+	if step == 'predictor':
+
+		if flux == 0:
+			tau_xy = mu * (compute_y_deriv(U,x,y,2) + compute_x_deriv(V,x,y,1))
+		elif flux == 1:
+			tau_xy = mu * (compute_y_deriv(U,x,y,2) + compute_x_deriv(V,x,y,0))
+		else:
+			raise Exception('Invalid Flux Direction')
 
 	elif step == 'corrector':
 
+		if flux == 0:
+			tau_xy = mu * (compute_y_deriv(U,x,y,1) + compute_x_deriv(V,x,y,2))
+		elif flux == 1:
+			tau_xy = mu * (compute_y_deriv(U,x,y,0) + compute_x_deriv(V,x,y,2))
+		else:
+			raise Exception('Invalid Flux Direction')
 
 	else:
-		raise('Invalid step')
+		raise Exception('Invalid Step')
 
+	return tau_xy
+
+def compE(Q,x,y,mu,gamma,step):
+	nx = np.shape(x,0)
+	ny = np.shape(y,1)
+	E = np.zeros([nx,ny,nvars])
+
+	Rho, U, V, P = ConsToPrim(Q,gamma)
+
+	tau_xx = Tauxx(U,x,y,mu,step)
+	tau_xy = Tauxy(U,V,x,y,mu,0,step)
+
+	E[:,:,0] = Rho * U
+	E[:,:,1] = Rho * U**2 + P - tau_xx
+	E[:,:,2] = Rho * U * V - tau_xy
+	E[:,:,3] = (Q[:,:,-1] + P) * U - U * tau_xx - V * tau_xy
+
+	return E
+
+def compF(Q,x,y,mu,gamma,step):
+	nx = np.shape(x,0)
+	ny = np.shape(y,0)
+	F = np.zeros([nx,ny,nvars])
+
+	Rho, U, V, P = ConsToPrim(Q,gamma)
+
+	tau_yy = Tauyy(V,x,y,mu,step)
+	tau_xy = Tauxy(U,V,x,y,mu,1,step)
+
+	F[:,:,0] = Rho * V
+	F[:,:,1] = Rho * U * V - tau_xy
+	F[:,:,2] = Rho * V**2 + P - tau_yy
+	F[:,:,3] = (Q[:,:,-1] + P) * V - U * tau_xy - V * tau_yy
+
+	return F
+
+def compRHS(Q,x,y,dt,mu,gamma,step):
+	nx = np.shape(x,0)
+	ny = np.shape(y,1)
+	lo_x = 1
+	hi_x = nx-1
+	lo_y = 1
+	hi_y = ny-1
+
+
+	# format of Q[x,y,0-3] 
+	if step == 'predictor':
+		E = compE(Q,x,y,mu,gamma,step)
+		F = compF(Q,x,y,mu,gamma,step)
+
+		dEdx = compute_x_deriv(E,x,y,0)
+		dFdy = compute_y_deriv(F,x,y,1)
+
+		rhs = -1 * (dEdx + dFdy)
+	elif step == 'corrector':
+		E = compE(Q,x,y,mu,gamma,step)
+		F = compF(Q,x,y,mu,gamma,step)
+
+		dEdx = compute_x_deriv(E,x,y,1)
+		dFdy = compute_y_deriv(F,x,y,0)
+
+		rhs = -1 * (dEdx + dFdy)
+	else:
+		raise Exception('Invalid step')
+
+	return rhs
+# Compute RHS for drho/dt
+
+# def compute_rhs_rho(Rho,U,rhs_rho,dir):
+
+# 	# only apply on interior of domain, BCs handled elsewhere
+
+# 	if dir == 'forward':
+# 		for ii in range(1,nx-1):
+# 			term[ii] = -1 * (Rho[ii+1] * U[ii+1] - Rho[ii] * U[ii]) / (xg[ii+1] - xg[ii])
+# 	elif dir == 'backward':
+# 		for ii in range(1,nx-1):
+# 			term[ii] = -1 * (Rho[ii] * U[ii] - Rho[ii-1] * U[ii-1]) / (xg[ii] - xg[ii-1])
+# 	else:
+# 		for ii in range(1,nx-1):
+# 			term[ii] = -1 * (Rho[ii+1] * U[ii+1] - Rho[ii-1] * U[ii-1]) / (xg[ii+1] - xg[ii-1])
+
+# 	rhs_rho[1:nx-1] += term
+
+# 	return rhs_rho
+
+# def compute_rhs_rhoU(Rho,U,P,rhs_rhoU,dir):
+
+# 	term1 = np.array([nx-2,1])
+# 	term2 = np.array([nx-2,1])
+# 	term3 = np.array([nx-2,1])
+
+# 	# only apply on interior of domain, BCs handled elsewhere
+
+# 	if dir == 'forward':
+# 		for ii in range(1,nx-1):
+# 			term1[ii] = -1 * (Rho[ii+1] * U[ii+1]**2 - Rho[ii] * U[ii]**2) / (xg[ii+1] - xg[ii]) # rho u^2
+# 			term2[ii] = -1 * (P[ii+1] - P[ii]) / (xg[ii+1] - xg[ii]) # pressure
+# 			term3[ii] = 2 * mu * (U[ii+1] - 2 * U[ii] + U[ii-1]) / (xg[ii+1] - xg[ii-1])**2 # d/dx(tau_xx) 
+# 	elif dir == 'backward':
+# 		for ii in range(1,nx-1):
+# 			term1[ii] = -1 * (Rho[ii+1] * U[ii+1]**2 - Rho[ii-1] * U[ii-1]**2) / (xg[ii+1] - xg[ii-1]) # rho u^2
+# 			term2[ii] = -1 * (P[ii+1] - P[ii-1]) / (xg[ii+1] - xg[ii-1]) # pressure
+# 			term3[ii] = 2 * mu * (U[ii+1] - 2 * U[ii] + U[ii-1]) / (xg[ii+1] - xg[ii-1])**2 # d/dx(tau_xx) 
+# 	else:
+# 		for ii in range(1,nx-1):
+# 			term1[ii] = -1 * (Rho[ii+1] * U[ii+1]**2 - Rho[ii-1] * U[ii-1]**2) / (xg[ii+1] - xg[ii-1]) # rho u^2
+# 			term2[ii] = -1 * (P[ii+1] - P[ii-1]) / (xg[ii+1] - xg[ii-1]) # pressure
+# 			term3[ii] = 2 * mu * (U[ii+1] - 2 * U[ii] + U[ii-1]) / (xg[ii+1] - xg[ii-1])**2 # d/dx(tau_xx) 
+
+# 	rhs_rhoU[1:nx-1] += term1 + term2 + term3
+
+# 	return rhs_rhoU
+
+# def compute_rhs_Et(Et,U,P,rhs_Et):
+
+# 	term1 = np.array([nx-2,1])
+# 	term2 = np.array([nx-2,1])
+
+# 	for ii in range(1,nx-1):
+# 		term1[ii] = U[ii] * ( (Et[ii+1] - Et[ii-1]) / (xg[ii+1] - xg[ii-1]) + (P[ii+1] - P[ii-1]) / (xg[ii+1] - xg[ii-1]) ) +\
+# 		(Et[ii] + P[ii]) * (U[ii+1] - U[ii-1]) / (xg[ii+1] - xg[ii-1]) # d/dx ((Et + p) * u)
+# 		term2[ii] = 2 * mu * ( ((U[ii+1] - U[ii-1]) / (xg[ii+1] - xg[ii-1]))**2 +  U[ii] * \
+# 		(U[ii+1] - 2 * U[ii] + U[ii-1]) / (xg[ii+1] - xg[ii-1])**2 ) # d/dx (u * tau_xx)
+
+# 	rhos_E[1:nx-1] += term1 + term2
+
+# 	return rhos_Et
