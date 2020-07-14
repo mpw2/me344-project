change to grid arrays, fix CFL calculation, change global to common

src/initialization.py
@@ -1,5 +1,5 @@
 
-import global
+import common
 
 def initialize():
     # Output variables
@@ -16,13 +16,11 @@ def initialize():
     P = np.zeros((nx,ny))
 
     # Build the grid
-    xg_temp = np.linspace(0,Lx,nx,endpoint=False) + (0.5*Lx)/nx
-    if ( ny % 2 == 0 ):
-        yg_temp = np.linspace(0,Ly/2,ny/2,endpoint=False) + (0.5*Ly)/(ny)
-        yg_temp2 = -np.flip(yg_temp)
-        yg_temp = np.append(yg_temp2,yg_temp)
+    xg_temp = np.linspace(0,Lx,nx)
+    if ( ny % 2 == 1 ):
+        yg_temp = np.linspace(-Ly/2,Ly/2,ny)
     else:
-        raise Exception("Use even values for ny")
+        raise Exception("Use odd values for ny")
     # use shape to allow easy commuting with field variables
     xg = np.ndarray((nx,1))
     yg = np.ndarray((1,ny))

src/time_integration.py
@@ -1,17 +1,16 @@
 
-import global
+import common
 
 def compute_timestep_maccormack():
     Qo = Q.copy()
     
-    
     k1 = compRHS(Q,x,y,'predictor')
-    Q[1:nx-1,1:ny-1,:] = Q[1:nx-1,1:ny-1,:] + dt*k1
+    Q[1:nx-1,1:ny-1,:] = Q[1:nx-1,1:ny-1,:] + dt*k1[1:nx-1,1:ny-1,:] 
     
     apply_boundary_conditions()
 
     k2 = compRHS(Q,x,y,'corrector')
-    Q[1:nx-1,1:ny-1,:] = Qo[1:nx-1,1:ny-1,:] + dt*( k1 + k2 )/2.0
+    Q[1:nx-1,1:ny-1,:]  = Qo[1:nx-1,1:ny-1,:] + dt*( k1[1:nx-1,1:ny-1,:] + k2[1:nx-1,1:ny-1,:] )/2.0
     
     apply_boundary_conditions() 
 
@@ -22,12 +21,22 @@ def compute_dt():
     global dt
     
     Rho_,U_,V_,P_ = ConsToPrim(Q,gamma)
-    Ucfl = np.sqrt( U_**2 + V_**2 )
-    Ucfl = np.min(Ucfl)
     a0 = np.sqrt( gamma*P/Rho )
-    Ucfl = Ucfl + a0
-    dx = np.min( np.min( np.diff(xg) ), np.min( np.diff(yg) ) )
-    dt = CFL_ref * dx / Ucfl
+    
+    Ur = np.abs(U_ + a0)
+    Ul = np.abs(U_ - a0)
+    U_ = np.maximum(Ur,Ul)
+    
+    Vr = np.abs(V_ + a0)
+    Vl = np.abs(V_ - a0)
+    V_ = np.maximum(Vr,Vl)
+     
+    dx = np.gradient(xg)
+    dy = np.gradient(yg)    
+    
+    dt = CFL_ref / (U_/dx + V_/dy)
+
+    dt = np.min(dt)
 
 
 