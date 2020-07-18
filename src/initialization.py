c56ca69d168bdaba893e3ff5ee00d2356bab4a2c
Carlos Gonzalez <gonzalez.a.carlos09@gmail.com>
2020-07-18 15:49:53 -0700

    updated initiliaziation.py to be 3d

--- src/initialization.py
+++ src/initialization.py
@@ -6,13 +6,13 @@ import boundary_conditions as bc
 def initialize():
 
     # Transport variable arrays
-    g.Q = np.zeros((g.nx+1,g.ny+1,g.NVARS))
+    g.Q = np.zeros((g.nx+1,g.ny+1,g.nz+1,g.NVARS))
     
     # Primitive variable arrays
-    g.Rho = np.zeros((g.nx+1,g.ny+1))
-    g.U = np.zeros((g.nx+1,g.ny+1))
-    g.V = np.zeros((g.nx+1,g.ny+1))
-    g.P = np.zeros((g.nx+1,g.ny+1))
+    g.Rho = np.zeros((g.nx+1,g.ny+1,g.nz+1))
+    g.U = np.zeros((g.nx+1,g.ny+1,g.nz+1))
+    g.V = np.zeros((g.nx+1,g.ny+1,g.nz+1))
+    g.P = np.zeros((g.nx+1,g.ny+1,g.nz+1))
 
     # Time variables
     g.t = 0.0
@@ -24,11 +24,19 @@ def initialize():
         yg_temp = np.linspace(-g.Ly/2,g.Ly/2,g.ny+1)
     else:
         raise Exception("Use odd values for ny")
+
+    if (g.nz % 2 == 1):
+        zg_temp = np.linspace(-g.Lz/2,g.Lz/2,g.nz+1)
+    else:
+        raise Exception('Use odd values for nz')
+
     # use shape to allow easy commuting with field variables
-    g.xg = np.ndarray((g.nx+1,1))
-    g.yg = np.ndarray((1,g.ny+1))
-    g.xg[:,0] = xg_temp
-    g.yg[0,:] = yg_temp
+    g.xg = np.ndarray((g.nx+1,1,1))
+    g.yg = np.ndarray((1,g.ny+1,1))
+    g.zg = np.ndarray((1,1,g.nz+1))
+    g.xg[:,0,0] = xg_temp
+    g.yg[0,:,0] = yg_temp
+    g.zg[0,0,:] = zg_temp
     
 
     g.Rho_inf = g.P_inf / (g.R_g * g.T_inf)
@@ -37,23 +45,27 @@ def initialize():
     # calculate the sponge damping factors
     x_sponge = g.x_sponge*g.Lx
     y_sponge = g.y_sponge*g.Ly/2
-    wall_dist = np.zeros((g.nx+1,g.ny+1,1))
+    z_sponge = g.z_sponge*g.Lz/2
+    wall_dist = np.zeros((g.nx+1,g.ny+1,g.nz+1,1))
     for i in range(g.nx+1):
         for j in range(g.ny+1):
-            # calculate nondim dist from desired boundaries
-            x1 = abs(g.Lx - g.xg[i,0])/x_sponge
-            y0 = abs(g.yg[0,j] + g.Ly/2)/y_sponge
-            y1 = abs(g.Ly/2 - g.yg[0,j])/y_sponge
-            # take minimum distance within sponge length
-            wall_dist[i,j,0] = np.min([x1,y0,y1,1.0])
+            for k in range(g.nz+1):
+                # calculate nondim dist from desired boundaries
+                x1 = abs(g.Lx - g.xg[i,0,0])/x_sponge
+                y0 = abs(g.yg[0,j,0] + g.Ly/2)/y_sponge
+                y1 = abs(g.Ly/2 - g.yg[0,j,0])/y_sponge
+                z0 = abs(g.zg[0,0,k] + g.Lz/2)/z_sponge
+                z1 = abs(g.Lz/2 - g.zg[0,0,k])/z_sponge
+                # take minimum distance within sponge length
+                wall_dist[i,j,k,0] = np.min([x1,y0,y1,z0,z1,1.0])
     g.sponge_fac = 1.0 - wall_dist**g.a_sponge
     
     # Calculate the reference condition
-    g.Qref = np.zeros((1,1,g.NVARS))
-    g.Qref[:,:,0] = g.Rho_inf
-    g.Qref[:,:,1] = 0.0
-    g.Qref[:,:,2] = 0.0
-    g.Qref[:,:,3] = g.P_inf / (g.gamma - 1.0)
+    g.Qref = np.zeros((1,1,1,g.NVARS))
+    g.Qref[:,:,:,0] = g.Rho_inf
+    g.Qref[:,:,:,1] = 0.0
+    g.Qref[:,:,:,2] = 0.0
+    g.Qref[:,:,:,3] = g.P_inf / (g.gamma - 1.0)
     
     # --------------------------
     # Initialize the flow field
