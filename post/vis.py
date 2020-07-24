c5f4b033d693f991e85255e538b9265efdf4a6de
Michael Whitmore <mpw2@stanford.edu>
2020-07-23 19:32:27 -0700

    linting with flake8

--- post/vis.py
+++ post/vis.py
@@ -6,18 +6,19 @@ import sys
 import pickle
 import os
 
+
 def main():
 
     if len(sys.argv) == 2:
 
         dataFile = sys.argv[1]
 
-        # f = open(dataFile,'rb')
+        # f = open(dataFile, 'rb')
         # saveVars = np.load(f)
 
         # print(type(saveVars))
 
-        with open(dataFile,'rb') as f:
+        with open(dataFile, 'rb') as f:
             saveVars = pickle.load(f)
 
         # saveVars = [x, y, Q]
@@ -32,37 +33,36 @@ def main():
         ny = y.size
         nz = z.size
 
-        x = np.tile(x,(1,ny,nz))
-        y = np.tile(y,(nx,1,nz))
-        z = np.tile(z,(nx,ny,1))
+        x = np.tile(x, (1, ny, nz))
+        y = np.tile(y, (nx, 1, nz))
+        z = np.tile(z, (nx, ny, 1))
 
-        Rho = Q[:,:,:,0]
-        U = Q[:,:,:,1] / Q[:,:,:,0]
-        V = Q[:,:,:,2] / Q[:,:,:,0]
-        W = Q[:,:,:,3] / Q[:,:,:,0]
+        Rho = Q[:, :, :, 0]
+        U = Q[:, :, :, 1] / Q[:, :, :, 0]
+        V = Q[:, :, :, 2] / Q[:, :, :, 0]
+        W = Q[:, :, :, 3] / Q[:, :, :, 0]
 
-        #print(Q)
-        #Rho, U, V, P = eq.ConsToPrim(Q)
+        # print(Q)
+        # Rho, U, V, P = eq.ConsToPrim(Q)
 
-
-        fig = plt.figure()
-        plt.contourf(x,y,Rho,100)
+        plt.figure()
+        plt.contourf(x, y, Rho, 100)
         plt.xlabel('x')
         plt.ylabel('y')
         plt.title(r'$\rho$')
         plt.colorbar()
         plt.savefig('images/rho.png')
 
-        fig = plt.figure()
-        plt.contourf(x,y,U,100)
+        plt.figure()
+        plt.contourf(x, y, U, 100)
         plt.xlabel('x')
         plt.ylabel('y')
         plt.title(r'$U$')
         plt.colorbar()
         plt.savefig('images/u.png')
 
-        fig = plt.figure()
-        plt.contourf(x,y,V,100)
+        plt.figure()
+        plt.contourf(x, y, V, 100)
         plt.xlabel('x')
         plt.ylabel('y')
         plt.title(r'$V$')
@@ -76,7 +76,7 @@ def main():
             # split data file to get step number
             step = dataFile.split(".")[1]
 
-            with open('./out/' + dataFile,'rb') as f:
+            with open('./out/' + dataFile, 'rb') as f:
                 saveVars = pickle.load(f)
 
             x = saveVars[0]
@@ -89,65 +89,66 @@ def main():
             ny = y.size
             nz = z.size
 
-            x = np.tile(x,(1,ny,nz))
-            y = np.tile(y,(nx,1,nz))
-            z = np.tile(z,(nx,ny,1))
+            x = np.tile(x, (1, ny, nz))
+            y = np.tile(y, (nx, 1, nz))
+            z = np.tile(z, (nx, ny, 1))
 
             gamma = 1.4
 
-            Rho = Q[:,:,:,0]
-            U = Q[:,:,:,1] / Q[:,:,:,0]
-            V = Q[:,:,:,2] / Q[:,:,:,0]
-            W = Q[:,:,:,3] / Q[:,:,:,0]
-            P = (gamma-1) * (Q[:,:,:,4] - 0.5 / Q[:,:,:,0] * (Q[:,:,:,1] + Q[:,:,:,2] + Q[:,:,:,3])**2)
-            Phi = Q[:,:,:,5] / Q[:,:,:,0]
+            Rho = Q[:, :, :, 0]
+            U = Q[:, :, :, 1] / Q[:, :, :, 0]
+            V = Q[:, :, :, 2] / Q[:, :, :, 0]
+            W = Q[:, :, :, 3] / Q[:, :, :, 0]
+            P = (gamma - 1) * (Q[:, :, :, 4] - 0.5 / Q[:, :, :, 0] *
+                               (Q[:, :, :, 1] + Q[:, :, :, 2] +
+                                Q[:, :, :, 3])**2)
+            Phi = Q[:, :, :, 5] / Q[:, :, :, 0]
 
-            #print(Q)
-            #Rho, U, V, P = eq.ConsToPrim(Q)
+            # print(Q)
+            # Rho, U, V, P = eq.ConsToPrim(Q)
 
-
-            fig = plt.figure()
-            plt.contourf(x[:,:,0],y[:,:,0],Rho[:,:,int(nz/2)],100)
+            plt.figure()
+            plt.contourf(x[:, :, 0], y[:, :, 0], Rho[:, :, int(nz/2)], 100)
             plt.xlabel('x')
             plt.ylabel('y')
             plt.title(r'$\rho$')
             plt.colorbar()
             plt.savefig('images/rho/rho' + '.' + step + '.png')
 
-            fig = plt.figure()
-            plt.contourf(x[:,:,0],y[:,:,0],U[:,:,int(nz/2)],100)
+            plt.figure()
+            plt.contourf(x[:, :, 0], y[:, :, 0], U[:, :, int(nz/2)], 100)
             plt.xlabel('x')
             plt.ylabel('y')
             plt.title(r'$U$')
             plt.colorbar()
             plt.savefig('images/u/u' + '.' + step + '.png')
 
-            fig = plt.figure()
-            plt.contourf(x[:,:,0],y[:,:,0],V[:,:,int(nz/2)],100)
+            plt.figure()
+            plt.contourf(x[:, :, 0], y[:, :, 0], V[:, :, int(nz/2)], 100)
             plt.xlabel('x')
             plt.ylabel('y')
             plt.title(r'$V$')
             plt.colorbar()
             plt.savefig('images/v/v' + '.' + step + '.png')
 
-            fig = plt.figure()
-            plt.contourf(x[:,:,0],y[:,:,0],W[:,:,int(nz/2)],100)
+            plt.figure()
+            plt.contourf(x[:, :, 0], y[:, :, 0], W[:, :, int(nz/2)], 100)
             plt.xlabel('x')
             plt.ylabel('y')
             plt.title(r'$W$')
             plt.colorbar()
             plt.savefig('images/w/w' + '.' + step + '.png')
 
-            fig = plt.figure()
-            plt.contourf(x[:,:,0],y[:,:,0],P[:,:,int(nz/2)],100)
+            plt.figure()
+            plt.contourf(x[:, :, 0], y[:, :, 0], P[:, :, int(nz/2)], 100)
             plt.xlabel('x')
             plt.ylabel('y')
             plt.title(r'$P$')
             plt.colorbar()
             plt.savefig('images/p/p' + '.' + step + '.png')
 
-            fig = plt.figure()
-            plt.contourf(x[:,:,0],y[:,:,0],Phi[:,:,int(nz/2)],100)
+            plt.figure()
+            plt.contourf(x[:, :, 0], y[:, :, 0], Phi[:, :, int(nz/2)], 100)
             plt.xlabel('x')
             plt.ylabel('y')
             plt.title(r'$\phi$')
@@ -160,6 +161,8 @@ def main():
             print('')
 
 
-
 if __name__ == "__main__":
     main()
+
+
+#
